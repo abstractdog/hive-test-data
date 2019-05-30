48e07fdb36fc3f2e58d14c05f1088085820e9cca
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2019-05-30 13:23:12 +0000

    description as string

--- load.sql
+++ load.sql
@@ -1,15 +1,17 @@
-CREATE TABLE IF NOT EXISTS sample (id int, desc varchar(200), name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
+CREATE TABLE IF NOT EXISTS sample
+(id int, desc string, name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 stored as textfile;
 LOAD DATA LOCAL INPATH '/tmp/sample.csv' OVERWRITE INTO TABLE sample;
 
 
-CREATE TABLE IF NOT EXISTS sample_orc(id int, desc varchar(200), name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
+CREATE TABLE IF NOT EXISTS sample_orc
+(id int, desc string, name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
 STORED AS ORC;
 INSERT INTO TABLE sample_orc SELECT * FROM sample;
 
 CREATE TABLE IF NOT EXISTS sample_orc_part
-(id int, desc varchar(200), name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, department varchar (100), ratio decimal)
+(id int, desc string, name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, department varchar (100), ratio decimal)
 PARTITIONED BY (region varchar (100))
 STORED AS ORC;
 
