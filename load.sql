ca712f0a072d8049727ddb1c994781ec8bebbef6
Bodor Laszlo <bodorlaszlo0202@gmail.com>
2019-09-27 17:20:10 +0200

    Update load.sql: new table names + bucketing

--- load.sql
+++ load.sql
@@ -1,20 +1,21 @@
-CREATE TABLE IF NOT EXISTS sample
-(id int, desc string, name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
+CREATE TABLE IF NOT EXISTS sample_txt_tmp
+(id int, desc string, name varchar(50), category int, price1 float, price2 double, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 stored as textfile;
 LOAD DATA INPATH '/tmp/sample.csv' OVERWRITE INTO TABLE sample;
 
 
-CREATE TABLE IF NOT EXISTS sample_orc
-(id int, desc string, name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
+CREATE TABLE IF NOT EXISTS sample_orc_tmp
+(id int, desc string, name varchar(50), category int, price1 float, price2 double, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
 STORED AS ORC;
-INSERT INTO TABLE sample_orc SELECT * FROM sample;
+INSERT INTO TABLE sample_orc SELECT * FROM sample_txt_tmp;
 
-CREATE TABLE IF NOT EXISTS sample_orc_part
-(id int, desc string, name varchar(50), category int, price1 decimal, price2 decimal, vat decimal, department varchar (100), ratio decimal)
+CREATE TABLE IF NOT EXISTS sample
+(id int, desc string, name varchar(50), category int, price1 float, price2 double, vat decimal, department varchar (100), ratio decimal)
 PARTITIONED BY (region varchar (100))
+CLUSTERED BY (department) INTO 32 BUCKETS
 STORED AS ORC;
 
 set hive.exec.dynamic.partition.mode=nonstrict;
-INSERT INTO sample_orc_part PARTITION (region) SELECT id, desc, name, category, price1, price2, vat, department, ratio, region FROM sample_orc;
+INSERT INTO sample_orc_part PARTITION (region) SELECT id, desc, name, category, price1, price2, vat, department, ratio, region FROM sample_orc_tmp;
 
