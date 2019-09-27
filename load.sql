84793618e71d66921c289ee52d5eddbb21f5bb01
Bodor Laszlo <bodorlaszlo0202@gmail.com>
2019-09-27 17:21:38 +0200

    load.sql: fixed table names

--- load.sql
+++ load.sql
@@ -2,13 +2,13 @@ CREATE TABLE IF NOT EXISTS sample_txt_tmp
 (id int, desc string, name varchar(50), category int, price1 float, price2 double, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 stored as textfile;
-LOAD DATA INPATH '/tmp/sample.csv' OVERWRITE INTO TABLE sample;
+LOAD DATA INPATH '/tmp/sample.csv' OVERWRITE INTO TABLE sample_txt_tmp;
 
 
 CREATE TABLE IF NOT EXISTS sample_orc_tmp
 (id int, desc string, name varchar(50), category int, price1 float, price2 double, vat decimal, region varchar (100), department varchar (100), ratio decimal) 
 STORED AS ORC;
-INSERT INTO TABLE sample_orc SELECT * FROM sample_txt_tmp;
+INSERT INTO TABLE sample_orc_tmp SELECT * FROM sample_txt_tmp;
 
 CREATE TABLE IF NOT EXISTS sample
 (id int, desc string, name varchar(50), category int, price1 float, price2 double, vat decimal, department varchar (100), ratio decimal)
@@ -17,5 +17,5 @@ CLUSTERED BY (department) INTO 32 BUCKETS
 STORED AS ORC;
 
 set hive.exec.dynamic.partition.mode=nonstrict;
-INSERT INTO sample_orc_part PARTITION (region) SELECT id, desc, name, category, price1, price2, vat, department, ratio, region FROM sample_orc_tmp;
+INSERT INTO sample PARTITION (region) SELECT id, desc, name, category, price1, price2, vat, department, ratio, region FROM sample_orc_tmp;
 
